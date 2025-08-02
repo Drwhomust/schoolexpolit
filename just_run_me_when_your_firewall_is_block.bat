4a89ed427d320c65ff7eadc95b599d3882970312
Drwhomust <rabbids8008@gmail.com>
2025-08-02 00:42:07 -0700

    reworking everything

--- just_run_me_when_your_firewall_is_block.bat
+++
@@ -1,6 +0,0 @@
-tasklist
-taskkill /F /IM cisco.exe /T
-taskkill /F /IM umbrella.exe /T
-echo done with unblocking firewall
-echo support your local furries and therians!
-echo feel free to come to the park to do qards with us therians
