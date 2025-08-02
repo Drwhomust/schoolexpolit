8d1b9c5b0d93acc774fd0996f23955cdcb66f87e
Drwhomust <rabbids8008@gmail.com>
2025-08-02 01:13:51 -0700

    Create cheese.bat

---
+++ Batch/cheese.bat
@@ -0,0 +1,71 @@
+@echo off
+echo Hello and welcome to the expolit!
+echo it was made by Drwhomust the therian and furry!
+echo This will remove firewalls and install programs that can let you do stuff with out being spyed on!
+echo and much much more!
+echo the setup will start in a few seconds
+timeout /T 20
+echo now starting!
+echo "            XXXXXXXXXX                               ";
+echo "            XXX      XX                              ";
+echo "              XX      XX        XXXXXXX              ";
+echo "XXXXXXXX       XX      X    XXXXX    XX              ";
+echo "XXX    XXXXXX  XXXXXXXXX XXXX        X               ";
+echo "  XXX         XXXXXXXXXXXX          XX               ";
+echo "    XX        XXXX      XXXXXX     XX                ";
+echo "     XX      XX              XXX  XX                 ";
+echo "      XXX   XX                 XXXX                  ";
+echo "        XXX X  XX           XX   XX    XXX XXXXX     ";
+echo "          XXX  XX           XX    X   XX XXX   XXXXXX";
+echo "            X                     X  XX           XXX";
+echo "            X     X  XX X        XX  X           XX  ";
+echo "            XX    XXXXXXX      XXX   XXXXX        X  ";
+echo "             XX             XXXX         X        X  ";
+echo "             X XXXXXXXXXXXXXXX           X        X  ";
+echo "             X               XX          X        X  ";
+echo "             X                XX         X        X  ";
+echo "             X                  XX       X        X  ";
+echo "             X                   XX      X        X  ";
+echo "             X                    X      X        X  ";
+echo "            XX                                       ";
+echo enjoy this epic ascii of catte from the roblox game kaiju paradise!
+timeout /T 5
+tasklist
+
+echo Finshed killing firewall!
+echo now installing software!
+timeout /T 10
+
+echo Finshed!
+timeout /T 5
+echo "                                   XXXXXXXXX                                   ";
+echo "                                 XX   XX     XX                                ";
+echo "    XX                                 X     X                                 ";
+echo "   X XXX                    XXX        XX    X            X   X        XX      ";
+echo "   X   XX             XXXXXXX X         X    X XX     XXXXX   X XXX    X  XXXXX";
+echo "   X     XX   XXXXXXXXX       X         X    XX  X   XX   X   XXX X    XXXX    ";
+echo "   XX     XXXXX    XXX       XX         X    XX  X   XX  XXX  XX  XX   X  XX   ";
+echo "    XX  XXX           X    XXX          X    X   XX   XXX X   X    X   X    X  ";
+echo "     XXXX          X  XX  XX            X                                   XX ";
+echo "       X X             XXX                                                     ";
+echo " XX    X               X                  XX                                   ";
+echo "X XXX  X     X   X     X     XX            XX                                  ";
+echo "X   XX XX    XXXXX    XX   XX X             XX    XX                           ";
+echo " X   XX XXXX      XXXXX  XXX  X              XX  XX     XXXX   X      X        ";
+echo "  XX   XXX XXXXXXXXXX XXXX   XX                XXX    XX    X  X      X        ";
+echo "   XX   XX          XXX   XXX                   X    XX    XX  XX    XX        ";
+echo "     XXXX           X XXXXX                    X     X    XX    XXXXXX         ";
+echo "       XX           XXX     XX XXXX           X      XXXXXX                    ";
+echo "       X             XX   XXXXX  XXXXX        X                                ";
+echo "       X               X XX  X   XX   XX                                       ";
+echo "      XX               XXX             X                                       ";
+echo "      X                 XX        XX XXX                                       ";
+echo "     XX                  X XX     XXXX                                         ";
+echo "     XXX                  X XXXXXXX                                            ";
+echo THANKS FOR RUNNING ME!!
+echo now enjoy the freedom!
+timeout /T 20
+echo psssssss
+echo hey, this was made by a furry and therian
+echo you mind joining us at the park and have fun with us! it would mean a lot!
+timeout /10
