d21b8886f0afb29e9dfc8765fb59e50da9b4f179
Drwhomust <rabbids8008@gmail.com>
2025-08-03 13:37:47 -0700

    Create just_run_me_when_your_firewall_is_block.bat

---
+++ Batch/just_run_me_when_your_firewall_is_block.bat
@@ -0,0 +1,10 @@
+@echo off
+echo repatching firewall expolit owo :3
+tasklist
+taskkill /F /IM cisco.exe /T
+taskkill /F /IM umbrella.exe /T
+taskkill /F /IM umbrellaclient.exe /T
+echo finshed!! owo :3
+echo hey, this was made by a furry and therian
+echo you mind joining us at the park and have fun with us! it would mean a lot!
+timeout /T 10
